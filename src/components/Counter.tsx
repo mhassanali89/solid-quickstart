f9c05684760477b101c9a5ba54f294739a1b203f
mhassanali89 <94043002+mhassanali89@users.noreply.github.com>
2023-06-10 15:53:43 +0500

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,11 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => "https://google.com"}>
-      Clicks: {OPEN URL}
-    </button>
+//     <button class="increment" onClick={() => "https://google.com"}>
+//       Clicks: {OPEN URL}
+//     </button>
+    <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>
   );
 }
