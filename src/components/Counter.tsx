8cda3dfdadf4f525ac612c27745c4d95455e4d44
mhassanali89 <94043002+mhassanali89@users.noreply.github.com>
2023-06-10 15:27:09 +0500

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,7 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count()}
+    <button class="increment" onClick={() => "https://google.com"}>
     </button>
   );
 }
